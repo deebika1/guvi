1f3cff25068fd7e3543cff596d9d66835625f0f8
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-15 20:19:34 +0530

    Create area_of_the_rectangle.py

---
+++ codekata/absolutebegineer/area_of_the_rectangle.py
@@ -0,0 +1,9 @@
+# program which is used to find the area of the recatangle
+def rectangle(len,bread):
+ area=len*bread
+ return area
+# get the length and breadth of the rectangle
+len=int(input())
+bread=int(input())
+res=rectangle(len,bread)
+print(res)
