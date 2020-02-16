c8a1166c28f90df9e0040b3d6ec0b5156ddfe0db
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-16 17:12:39 +0530

    Create sum_of_two_number.py

---
+++ codekata/absolutebegineer/sum_of_two_number.py
@@ -0,0 +1,11 @@
+# sum the two number by using function
+def sum(a,b):
+ c=a+b
+ return c
+# get the value of a and b from the user
+a=int(input())
+b=int(input())
+# call the function and the result is stored in res
+res=sum(a,b)
+# print the result value
+print(res)
