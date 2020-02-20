eb76c93f1f7605e6bd6aa6c46bc026e12bdac903
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-20 10:40:01 +0530

    Create not_of_the_given.py

---
+++ codeketa/basis/not_of_the_given.py
@@ -0,0 +1,8 @@
+# program which is used to print the not of the given number
+def not1(num):
+ res=~(num)
+ return res
+# get the num value form the user
+num=int(input())
+result=not1(num)
+print(result)
