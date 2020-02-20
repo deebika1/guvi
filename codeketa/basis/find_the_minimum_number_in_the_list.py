4a584571b7fe1b42eafb9d0190a213427f98febc
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-20 10:56:20 +0530

    Create find_the_minimum_number_in_the_list.py

---
+++ codeketa/basis/find_the_minimum_number_in_the_list.py
@@ -0,0 +1,7 @@
+#getting multiple inputs from single line and cast to list
+list = list(map(int, input("").split())) 
+#sort function
+# sort is the fuction which is used to sort the elements in the ascending order
+list.sort() 
+# list[0] index having the least number 
+print(list[0]) 
