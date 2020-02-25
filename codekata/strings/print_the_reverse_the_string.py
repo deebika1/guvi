f693910a1eed630a7e1e9fe93c19c8a3a5f5d5b6
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:13:31 +0530

    Create print_the_reverse_the_string.py

---
+++ codekata/strings/print_the_reverse_the_string.py
@@ -0,0 +1,12 @@
+# Given a string S, print the reverse of the string.
+string=input()
+rev=string[-1::-1]
+print(rev)
+
+#Given a string S, print the reverse of the string.
+#Input Size : |s| <= 100000 (ie do it in O(n) or O(log n) time complexity)
+#Sample Testcase :
+#INPUT
+#codekata
+#OUTPUT
+#atakedoc
