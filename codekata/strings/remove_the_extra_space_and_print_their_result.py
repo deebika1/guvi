3b73aaddc8cf6e60b8dcfab54521c65bb2a995d3
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:04:26 +0530

    Create remove_the_extra_space_and_print_their_result.py

---
+++ codekata/strings/remove_the_extra_space_and_print_their_result.py
@@ -0,0 +1,12 @@
+string=input()
+res=string.replace(" ",'')
+print(res)
+
+
+#Given a string/sentence remove all the spaces and print the result.
+#Input Size : |s| <= 1000000(complexity O(n))
+#Sample Testcase :
+#INPUT
+#guvi geeks
+#OUTPUT
+#guvigeeks
