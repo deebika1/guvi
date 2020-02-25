d0e7bf1ed9bfc2e57172889483977d413e34478d
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 19:58:57 +0530

    Create give_the_String_and_print_the_1st_and_3rd_character_in_the_given_String.py

---
+++ codekata/strings/give_the_String_and_print_the_1st_and_3rd_character_in_the_given_String.py
@@ -0,0 +1,12 @@
+#Given a string S, print the 1st and 3rd character of the string (chracter index starts from 1).
+string=input()
+res=string[0:4:2]
+print(res,end='')
+
+#Given a string S, print the 1st and 3rd character of the string (chracter index starts from 1).
+#Input Size : 1 <= N <= 100000
+#Sample Testcase :
+#INPUT
+#codekata
+#OUTPUT
+#cd
