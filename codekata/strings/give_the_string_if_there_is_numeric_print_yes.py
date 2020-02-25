a4d95e3c070e29128bdcf01be67f55083bed10e4
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:01:45 +0530

    Create give_the_string_if_there_is_numeric_print_yes.py

---
+++ codekata/strings/give_the_string_if_there_is_numeric_print_yes.py
@@ -0,0 +1,15 @@
+#Given a string S.Validate if a given string is numeric.print 'yes' if it is a numeric otherwise print 'no'.
+string=input()
+if(string.isdigit()):
+  print("yes")
+else:
+  print("no")
+
+
+
+#Given a string S.Validate if a given string is numeric.print 'yes' if it is a numeric otherwise print 'no'.
+#Sample Testcase :
+#INPUT
+#guvigeeks
+#OUTPUT
+#no
