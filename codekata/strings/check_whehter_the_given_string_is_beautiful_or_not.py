6468a59e5368f97902449d544eb7db01a91bdd86
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:12:32 +0530

    Create check_whehter_the_given_string_is_beautiful_or_not.py

---
+++ codekata/strings/check_whehter_the_given_string_is_beautiful_or_not.py
@@ -0,0 +1,18 @@
+#You are given a string ‘s’.Your task is to find whether string is beautiful or not.
+#A string is said to be beautiful whenever string is made up of only three characters. All the three characters must be distinct.
+#Print true if string is beautiful and false when it is not beautiful
+string=input()
+len1=len(string)
+if(len1==3):
+  print("1")
+else:
+  print("0")
+#You are given a string ‘s’.Your task is to find whether string is beautiful or not. A string is said to be beautiful whenever string is made up of only three characters. All the three characters must be distinct.Print true if string is beautiful and false when it is not beautiful
+#Input Description:
+#You are given a string
+#Output Description:
+#Print ‘1’ when string is beautiful and ‘0’ when it is not
+#Sample Input :
+#Aab
+#Sample Output :
+#1
