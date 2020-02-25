37b340134feee4b766d9b5951d431e8f56edbbda
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:00:24 +0530

    Create give_the_string_if_the_repetation_is_persent_print_yes.py

---
+++ codekata/strings/give_the_string_if_the_repetation_is_persent_print_yes.py
@@ -0,0 +1,34 @@
+# program whcih is used find the repetation of digits if it means print yes or not
+def repetation(num):
+ while(num>0):
+  t=num%10
+  all.append(t)
+  num=num//10
+#print(all)
+ return all
+# check1 function is used to check if any repetation of digits is persent or not
+def check1(res1):
+ len1=len(all)
+ res=len1-1
+#print(res)
+ for i in range(0,res+1):
+  for j in range(i+1,res+1):
+   if(all[i]==all[j]):
+    return "yes"
+ else:
+    return "no"
+# get the num value form the user
+num=int(input())
+all=[]
+res1=repetation(num)
+res2=check1(res1)
+print(res2)
+
+
+ 
+#Given a number N,check whether it has repeating digits in it.print 'yes' if it has repeating digits otherwise print 'no'.
+#Sample Testcase :
+#INPUT
+#11234
+#OUTPUT
+#yes
