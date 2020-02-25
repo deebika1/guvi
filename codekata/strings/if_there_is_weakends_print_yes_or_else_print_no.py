93518840300e16d299a287b15a2060dfb5eb8cfc
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:03:13 +0530

    Create if_there_is_weakends_print_yes_or_else_print_no.py

---
+++ codekata/strings/if_there_is_weakends_print_yes_or_else_print_no.py
@@ -0,0 +1,18 @@
+string=input()
+if(string=='saturday' or string=='sunday'):
+  print("yes")
+else:
+  print("no")
+  
+  
+  
+#Given a day, print 'yes' if it is a holiday otherwise print'no'.Assume that weekend days are holidays
+#Sample Testcase :
+#INPUT
+#saturday
+#OUTPUT
+#yes
+#INPUT
+#monday
+#OUTPUT
+#no
