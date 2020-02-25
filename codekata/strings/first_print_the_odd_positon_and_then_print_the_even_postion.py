bb261da105bf2c52f59d202ca4d40826c62480fa
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-02-25 20:15:01 +0530

    Create first_print_the_odd_positon_and_then_print_the_even_postion.py

---
+++ codekata/strings/first_print_the_odd_positon_and_then_print_the_even_postion.py
@@ -0,0 +1,11 @@
+s=input()
+print(s[0::2],end=' ')
+print(s[1::2],end='')
+
+
+#Given a string S, print 2 strings such that first string containing all characters in odd position(s) and other containing all characters in even position(s).
+#Sample Testcase :
+#INPUT
+#XCODE
+#OUTPUT
+#XOE CD
