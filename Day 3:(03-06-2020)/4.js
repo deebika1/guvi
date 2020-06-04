7186e141059340128a1fbd077974a8e89e8060b2
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-06-04 09:46:38 +0530

    Update 4.js

--- Day 3:(03-06-2020)/4.js
+++ Day 3:(03-06-2020)/4.js
@@ -24,3 +24,46 @@ var s=0;
 var odd=(array.reduce((s,n)=>s+=n));
 console.log(odd);
 /*4.4. Return all the prime numbers in an array*/
+var array=[1,2,3,4,5];
+var sum=(array)=>{
+    for(var i=0;i<array.length;i++)
+    {
+        var a=0;
+        if(array[i]>1)
+        {
+        for(var j=2;j<=array[i];j++)
+        {
+            if(array[i]%j==0 && array[i]!=j)
+            {
+                a=1
+                //console.log(array[i]);
+            }
+        }
+        if(a==0)
+        {
+            console.log(array[i]);
+        }
+        }
+    }
+}
+sum();
+/*4.5 Return all the palindromes in an array*/
+var str1="deebika";
+var palindrome1=(str)=>{
+    var s='';
+    for(var i=(str1.length)-1;i>=0;i--)
+    {
+        s+=str1[i];
+    }
+    //console.log(s);
+    if(s==str1)
+    {
+        console.log("palindrome");
+    }
+    else
+    {
+        console.log("not palindrome");
+    }
+}
+
+palindrome1();
