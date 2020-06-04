a8a89ca18ee67880417b6533c63c4f83ac2720cc
deebika1 <60967620+deebika1@users.noreply.github.com>
2020-06-04 10:38:11 +0530

    Update 3.js

--- Day 3:(03-06-2020)/3.js
+++ Day 3:(03-06-2020)/3.js
@@ -1,9 +1,9 @@
 /*3. Write a callback which is called
 3.1. if the give number is even*/
-function even(a,check)
+function even(a,call)
 {
 var res=0;
-if(check(a)==0)
+if(call(a)==0)
 {
 return "even"
 }
@@ -12,17 +12,17 @@ else
 return "odd"
 }
 }
-function checkCondition(a)
+function callback(a)
 {
 res=a%2
 return res;
 }
-var res=even(3,checkCondition);
+var res=even(3,callback);
 console.log(res);
 /*3.2. if the given number is prime*/
-function prime(a,check)
+function prime(a,call)
 {
-if(check(a)==a)
+if(call(a)==a)
 {
 return "prime"
 }
@@ -31,7 +31,7 @@ else
 return "not prime"
 }
 }
-function checkCondition(a)
+function callback(a)
 {
     for(var i=2;i<a;i++)
     {
@@ -49,18 +49,18 @@ function checkCondition(a)
     
 
 }
-var res=prime(5,checkCondition);
+var res=prime(5,callback);
 console.log(res);
 
 /*3.3. if the number is palindrome*/
 
 
-function palindrome(a,check)
+function palindrome(a,call)
 {
 var res=0;
 var s='';
 var n=[];
-if(check(a)==a)
+if(call(a)==a)
 {
 return "palindrome"
 }
@@ -69,11 +69,11 @@ else
 return "not a palindorme"
 }
 }
-function checkCondition(a)
+function callback(a)
 {
  //   console.log(a);
 var rev=a.split("").reverse();
 return rev;
 }
-var res=palindrome("hello",checkCondition);
+var res=palindrome("hello",callback);
 console.log(res);
